c376dc49a81be1c9904c1e09c5f241efead964fc
Samuel <samkoj@hotmail.co.uk>
2021-07-29 21:22:36 +0100

    send request on page load using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,11 +8,17 @@ function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	async function fetchMoviesHandler() {
+	// @3:00 https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599820#content
+	//    useCallback() allows us to save the fetchMoviesHandler function in some allocated space
+	//    which enables it to have the same pointer/reference, as objects e.g functions would not be compared and interpreted as identical
+	//    this allows us to add it as a dependency in the useEffect() hook.
+	//    In our case here, it's not really needed, an empy dependency array for useEffect would suffice,
+	//    however it would be needed if our fetchMoviesHandler would be using external state.
+	const fetchMoviesHandler = useCallback(async () => {
 		setIsLoading(true);
 		setError(null);
 		try {
-			const response = await fetch("https://swapi.dev/api/films/");
+			const response = await fetch("https://swapi.dev/api/film/");
 			if (!response.ok) throw new Error("Something went Wrong!");
 
 			const data = await response.json();
@@ -29,7 +35,11 @@ function App() {
 			setError(error.message);
 		}
 		setIsLoading(false);
-	}
+	}, []);
+
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
 
 	let content = <MoviesList movies={movies} />;
 	if (movies.length === 0) content = <p>Found no movies.</p>;
